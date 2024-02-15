b34167c4fcdc93f243b1cda2d58e02be8e3f561b
crear portafolio <tatianamantilla547@gmail.com>
2024-02-15 17:21:15 -0500

    cambios en gestion

--- GESTION DE TAREAS/index.js
+++ GESTION DE TAREAS/index.js
@@ -11,22 +11,29 @@ function agregar(prioridad) {
         nombre: document.getElementById("text").value,
         prioridad: prioridad,
         fecha: document.getElementById("Fec").value
-    } 
+    }
+if (datos.nombre === "" || datos.fecha === "") {
+        // prioridad = "";
+        alert("ingrese los datos")
+        limpiar()
+        
+    }
 
-  if (datos.nombre === "" && datos.fecha === "") {
-    prioridad="";
-    alert("ingrese los datos");
-  }
-    document.getElementById("text").value = "",
-        prioridad = "",
-        document.getElementById("Fec").value = ""
+  
 
     console.log(datos)
     dat.push(datos)
-    console.log(dat) 
- 
-  
-    }
+    console.log(dat)
+    document.getElementById("lista").textContent = "";
+    limpiar( )
+
+}
+function limpiar(){
+      document.getElementById("text").value = "",
+        prioridad = "",
+        document.getElementById("Fec").value = ""
+}
+
 
 function ordenarDatos() {
     let opcion = document.getElementById("op").value;
@@ -36,31 +43,26 @@ function ordenarDatos() {
             if (a.prioridad === "Alta" && b.prioridad === "Baja") {
                 return -1;
             }
-        } );
-    } else{
+        });
+    } else {
         dat.sort((a, b) => {
             if (a.prioridad === "Baja" && b.prioridad === "Alta") {
                 return -1;
             }
         });
     }
- 
-  
+    document.getElementById("lista").textContent="";
+
     pintar();
 }
 
 function pintar() {
-    let tableBody = document.getElementById("lista");
-    while (tableBody.firstChild) {
-        tableBody.removeChild(tableBody.firstChild);
-    }
-
     dat.forEach((item, i) => {
         let tr = document.createElement("tr");
         if (item.nombre !== "" && item.fecha !== "") {
-        let nombre = document.createElement("td");
-        nombre.textContent = item.nombre;
-        tr.appendChild(nombre);
+            let nombre = document.createElement("td");
+            nombre.textContent = item.nombre;
+            tr.appendChild(nombre);
         }
 
         if (item.nombre !== "" && item.fecha !== "") {
@@ -68,13 +70,25 @@ function pintar() {
             prioridad.textContent = item.prioridad;
             tr.appendChild(prioridad);
         }
-        
+
+        if (item.nombre !== "" && item.fecha !== "" ) {
+            let fecha = document.createElement("td");
+            fecha.textContent = item.fecha;
+            tr.appendChild(fecha);
+        }
         if (item.nombre !== "" && item.fecha !== "") {
-        let fecha = document.createElement("td");
-        fecha.textContent = item.fecha;
-        tr.appendChild(fecha);
+            let elimina = document.createElement("button")
+            elimina.textContent = "❌"
+      
+            tr.appendChild(elimina)
+            elimina.addEventListener("click", () => {
+                lista.removeChild(tr);
+                dat.splice(i, 1); // Eliminar el elemento correspondiente del array dat
+            });
+
         }
         document.getElementById("lista").appendChild(tr);
 
     });
 }
+
